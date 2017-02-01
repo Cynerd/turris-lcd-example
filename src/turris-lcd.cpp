518db760e2dbcfd0bf71ff3123e10f8a5d9140f8
Karel Kočí <karel.koci@nic.cz>
2017-02-01 17:45:45 +0100

    Set real i2c device on turris

--- src/turris-lcd.cpp
+++ src/turris-lcd.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 	// Initialize LCD
-	LiquidCrystal_I2C lcd("/dev/i2c-2", 0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+	LiquidCrystal_I2C lcd("/dev/i2c-7", 0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 	lcd.begin(20, 4);
 
 	lcd.on();
